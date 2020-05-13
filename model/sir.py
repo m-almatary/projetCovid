c5229d50c9443c4b6775546868e0fb5223781d86
Sedfik <ramos.val.91@gmail.com>
2020-05-13 14:20:06 +0200

    Ajout de la structure ainsi que du notebook

---
+++ model/sir.py
@@ -0,0 +1,29 @@
+
+
+class SIRModel():
+    """
+    Blabla sur SIR model
+    
+    TODO : equations
+
+    """
+    def __init__(self, beta_init=0.1, gamma_init=0.3):
+        self.beta_0 = beta_init
+        self.gamma_0 = gamma_init
+        self.beta_ = None
+        self.gamma_ = None
+
+
+    def fit(self, X, y):
+
+        return self
+
+
+    def predict(self, t):
+
+        return S, I, R
+
+
+    def foo(self, a, b):
+        pass
+
