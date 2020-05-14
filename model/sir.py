5547f60946660fd634260c1788fdc1fcd29590c5
Sedfik <ramos.val.91@gmail.com>
2020-05-14 11:08:03 +0200

    TO FIX: Ajout des fonctions du model SIR

--- model/sir.py
+++ model/sir.py
@@ -1,4 +1,5 @@
-
+from scipy.integrate import odeint
+from scipy import optimize, integrate
 
 class SIRModel():
     """
@@ -7,23 +8,29 @@ class SIRModel():
     TODO : equations
 
     """
-    def __init__(self, beta_init=0.1, gamma_init=0.3):
+
+    def __init__(self, SIR, t, N, beta_init=0.1, gamma_init=0.3):
+        self.SIR = SIR
+        self.t = [i for i in range(0,t)]
+        
         self.beta_0 = beta_init
         self.gamma_0 = gamma_init
         self.beta_ = None
         self.gamma_ = None
+        
+        self.sus = -self.beta_0 * self.SIR[0] * self.SIR[1] / N
+        self.rec = self.gamma_0 * self.SIR[1]
+        self.inf = -(self.sus+self.rec)
 
+    def fit(self, beta, gamma):
+        return integrate.odeint(self, self.SIR, self.t, args=(beta, gamma))[:,1]
 
-    def fit(self, X, y):
+    def predict(self):
+        popt, pcov = optimize.curve_fit(self.fit, self.t, self.SIR)
+        return popt, pcov
 
-        return self
+if __name__ == "__main__":
+    N = 64000000
+    sir = SIRModel((N,1,0),50,N)
 
-
-    def predict(self, t):
-
-        return S, I, R
-
-
-    def foo(self, a, b):
-        pass
-
+    print(sir.predict())
